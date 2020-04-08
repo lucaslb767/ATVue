f34464b714a37a4e6834a5026923c624d5b43f9d
lucaslb767 <lucaslbarrozo@gmail.com>
2020-04-08 07:09:01 -0300

    testev5

--- lucas_barrozo_dr3_at/src/components/EditCard.vue
+++ lucas_barrozo_dr3_at/src/components/EditCard.vue
@@ -2,14 +2,14 @@
   <div>
     <form @submit.prevent="onSubmit">
       <div><h2>Name:</h2></div>
-      <div><input type="text" v-model="todo_add.name" placeholder="Add title"></div>
+      <div><input type="text" v-model="todo_add.name" placeholder="Add title" required></div>
       <div>
         <h2>Text:</h2>
-        <input type="text" v-model="todo_add.text" placeholder="Add Text">
+        <input type="text" v-model="todo_add.text" placeholder="Add Text" required>
         <h2>Type:</h2>
-        <input type="text" v-model="todo_add.types[0]" placeholder="Add Type">
+        <input type="text" v-model="todo_add.types[0]" placeholder="Add Type" required>
         <h2>URL:</h2>
-        <input type="text" v-model="todo_add.imageUrl" placeholder="Add image url">
+        <input type="text" v-model="todo_add.imageUrl" placeholder="Add image url" required>
       </div>
 
       <div><input type="submit" value="Submit"></div>
